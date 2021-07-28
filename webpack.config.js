db23566b7fb76431f30c48dc23d863d40e8b95b2
sebast4an <szczypinskisebastian@gmail.com>
2021-07-28 06:26:44 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const webpack = require("webpack");
+const path = require('path');
+
+module.exports = {
+  target: 'web',
+  entry: './src/js/app.js',
+  output: {
+    filename: 'bundle.min.js',
+    path: path.resolve(__dirname, `dist`),
+    assetModuleFilename: 'assets/[name][ext][query]',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, `dist`),
+  },
+  devtool: 'source-map',
+  experiments: {
+    topLevelAwait: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [['@babel/transform-runtime']],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [['autoprefixer']],
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [['autoprefixer']],
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|svg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new ESLintPlugin(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'My App',
+      filename: 'index.html',
+      template: 'src/index.html',
+    }),
+  ],
+};
